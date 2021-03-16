f62b52f32f53b394e9d510339e498cb800613f41
courd <courdheadman@gmail.com>
2021-03-16 12:10:13 -0700

    initial commit - just deployed

---
+++ spawnitems/spawnitems.cs
@@ -0,0 +1,119 @@
+﻿//If you haven't done so yet, run the setup.bat file in your project/libs folder to acquire the needed references.
+using BepInEx;
+using R2API;
+using R2API.Utils;
+using RoR2;
+using UnityEngine;
+
+namespace SpawnItems
+{
+    //This is an example plugin that can be put in BepInEx/plugins/SpawnItems/SpawnItems.dll to test out.
+    //It's a small plugin that adds a relatively simple item to the game, and gives you that item whenever you press F2.
+
+    //This attribute specifies that we have a dependency on R2API, as we're using it to add our item to the game.
+    //You don't need this if you're not using R2API in your plugin, it's just to tell BepInEx to initialize R2API before this plugin so it's safe to use R2API.
+    [BepInDependency("com.bepis.r2api")]
+
+    //This attribute is required, and lists metadata for your plugin.
+    [BepInPlugin(
+        //The GUID should be a unique ID for this plugin, which is human readable (as it is used in places like the config). Java package notation is commonly used, which is "com.[your name here].[your plugin name here]"
+        "com.Courd.spawnitems",
+        //The name is the name of the plugin that's displayed on load
+        "spawnitems",
+        //The version number just specifies what version the plugin is.
+        "1.0.0")]
+    //Like seriously, if we see this boilerplate on thunderstore, we will deprecate this mod. Change that name!
+    //If you want to test package uploading in general, try using beta.thunderstore.io
+
+    //We will be using 3 modules from R2API: ItemAPI to add our item, ItemDropAPI to have our item drop ingame, and LanguageAPI to add our language tokens.
+    [R2APISubmoduleDependency(nameof(ItemAPI),nameof(ItemDropAPI),nameof(LanguageAPI))]
+    
+
+    //This is the main declaration of our plugin class. BepInEx searches for all classes inheriting from BaseUnityPlugin to initialize on startup.
+    //BaseUnityPlugin itself inherits from MonoBehaviour, so you can use this as a reference for what you can declare and use in your plugin class: https://docs.unity3d.com/ScriptReference/MonoBehaviour.html
+    public class spawnitems : BaseUnityPlugin
+    {
+
+        //The Update() method is run on every frame of the game.
+        public void Update()
+        {
+            //This if statement checks if the player has currently pressed F1.
+            if (Input.GetKeyDown(KeyCode.F1))
+            {
+                //Get the player body to use a position:	
+                var transform = PlayerCharacterMasterController.instances[0].master.GetBodyObject().transform;
+                //And then drop our defined item in front of the player.
+                //PickupDropletController.CreatePickupDroplet(PickupCatalog.FindPickupIndex(myItemDef.itemIndex), transform.position, transform.forward * 20f);
+
+                var dropList = Run.instance.availableTier1DropList;
+                var nextItem = Run.instance.treasureRng.RangeInt(0, dropList.Count);
+                PickupDropletController.CreatePickupDroplet(dropList[nextItem], transform.position, transform.forward * 20f);
+            }
+
+            //This if statement checks if the player has currently pressed F2.
+            if (Input.GetKeyDown(KeyCode.F2))
+            {
+                //Get the player body to use a position:	
+                var transform = PlayerCharacterMasterController.instances[0].master.GetBodyObject().transform;
+                //And then drop our defined item in front of the player.
+                //PickupDropletController.CreatePickupDroplet(PickupCatalog.FindPickupIndex(myItemDef.itemIndex), transform.position, transform.forward * 20f);
+
+                var dropList = Run.instance.availableTier2DropList;
+                var nextItem = Run.instance.treasureRng.RangeInt(0, dropList.Count);
+                PickupDropletController.CreatePickupDroplet(dropList[nextItem], transform.position, transform.forward * 20f);
+            }
+
+            //This if statement checks if the player has currently pressed F3.
+            if (Input.GetKeyDown(KeyCode.F3))
+            {
+                //Get the player body to use a position:	
+                var transform = PlayerCharacterMasterController.instances[0].master.GetBodyObject().transform;
+                //And then drop our defined item in front of the player.
+                //PickupDropletController.CreatePickupDroplet(PickupCatalog.FindPickupIndex(myItemDef.itemIndex), transform.position, transform.forward * 20f);
+
+                var dropList = Run.instance.availableTier3DropList;
+                var nextItem = Run.instance.treasureRng.RangeInt(0, dropList.Count);
+                PickupDropletController.CreatePickupDroplet(dropList[nextItem], transform.position, transform.forward * 20f);
+            }
+
+            //This if statement checks if the player has currently pressed F4.
+            if (Input.GetKeyDown(KeyCode.F4))
+            {
+                //Get the player body to use a position:	
+                var transform = PlayerCharacterMasterController.instances[0].master.GetBodyObject().transform;
+                //And then drop our defined item in front of the player.
+                //PickupDropletController.CreatePickupDroplet(PickupCatalog.FindPickupIndex(myItemDef.itemIndex), transform.position, transform.forward * 20f);
+
+                var dropList = Run.instance.availableBossDropList;
+                var nextItem = Run.instance.treasureRng.RangeInt(0, dropList.Count);
+                PickupDropletController.CreatePickupDroplet(dropList[nextItem], transform.position, transform.forward * 20f);
+            }
+
+            //This if statement checks if the player has currently pressed F5.
+            if (Input.GetKeyDown(KeyCode.F5))
+            {
+                //Get the player body to use a position:	
+                var transform = PlayerCharacterMasterController.instances[0].master.GetBodyObject().transform;
+                //And then drop our defined item in front of the player.
+                //PickupDropletController.CreatePickupDroplet(PickupCatalog.FindPickupIndex(myItemDef.itemIndex), transform.position, transform.forward * 20f);
+
+                var dropList = Run.instance.availableEquipmentDropList;
+                var nextItem = Run.instance.treasureRng.RangeInt(0, dropList.Count);
+                PickupDropletController.CreatePickupDroplet(dropList[nextItem], transform.position, transform.forward * 20f);
+            }
+
+            //This if statement checks if the player has currently pressed F6.
+            if (Input.GetKeyDown(KeyCode.F6))
+            {
+                //Get the player body to use a position:	
+                var transform = PlayerCharacterMasterController.instances[0].master.GetBodyObject().transform;
+                //And then drop our defined item in front of the player.
+                //PickupDropletController.CreatePickupDroplet(PickupCatalog.FindPickupIndex(myItemDef.itemIndex), transform.position, transform.forward * 20f);
+
+                var dropList = Run.instance.availableLunarDropList;
+                var nextItem = Run.instance.treasureRng.RangeInt(0, dropList.Count);
+                PickupDropletController.CreatePickupDroplet(dropList[nextItem], transform.position, transform.forward * 20f);
+            }
+        }
+    }
+}
